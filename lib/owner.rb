1caccbabe50af6f98621dea01f3f1d2c37b7a1a4
ktochow1 <ktochow1@binghamton.edu>
2018-12-02 22:27:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species 
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def pets
+    @owner_pets = Hash.new
+    @owner_pets = {:cats => [], :dogs => [], :fishes => []} 
+  end
+  
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    new_fish = @owner_pets[:fishes] = 1 
+    
+    
+    # @owner_pets[:fishes] << Fish.new(name)
+  end 
+    
 end
